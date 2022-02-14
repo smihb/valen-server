94342346b14ad1af9868196cee8cf4fde01153fb
Hayron Bomgnine <hayronbt@gmail.com>
2022-02-14 01:19:37 -0400

    Prueba para desplegar en aws

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
-const express = require('express')
+const express = require('express');
+const path = require('path');
 
 const app = express()
 
-app.get('/', (req, res)=>{
-    res.send('Hello World con express')
-})
+app.use(express.static(path.join(__dirname, '../public')))
 
-app.listen(3000);
-console.log('server on port 3000')
+app.listen(3000, ()=>console.log('Server on Port 3000'));
